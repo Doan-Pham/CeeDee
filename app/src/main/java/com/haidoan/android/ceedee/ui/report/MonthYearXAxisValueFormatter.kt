d7b7b80d8e597b007455e0b02993beace2f35494
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-10-16 11:35:44 +0700

    Add MonthYearXAxisValueFormatter for formatting bar chart's X Axis labels as month and year

---
+++ app/src/main/java/com/haidoan/android/ceedee/ui/report/MonthYearXAxisValueFormatter.kt
@@ -0,0 +1,24 @@
+package com.haidoan.android.ceedee.ui.report
+
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.formatter.ValueFormatter
+
+class MonthYearXAxisValueFormatter(
+    private val startMonth: Int = 10,
+    private val startYear: Int = 2022
+) : ValueFormatter() {
+
+    private val MONTH_COUNT_PER_YEAR = 12
+
+    override fun getAxisLabel(monthCountFromStart: Float, axis: AxisBase?): String {
+        val resultMonthUnnormalized = startMonth + monthCountFromStart.toInt()
+        var resultYear = startYear + resultMonthUnnormalized / MONTH_COUNT_PER_YEAR
+        var resultMonthNormalized = resultMonthUnnormalized % MONTH_COUNT_PER_YEAR
+
+        if (resultMonthNormalized == 0) {
+            resultYear--
+            resultMonthNormalized = MONTH_COUNT_PER_YEAR
+        }
+        return "$resultMonthNormalized/$resultYear"
+    }
+}
