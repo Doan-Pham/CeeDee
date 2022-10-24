c821a4afe756f1691592f90ac2b36cbe649f6117
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-10-24 21:50:42 +0700

    Implement LocalDate in MonthYearXAxisValueFormatter.kt

--- app/src/main/java/com/haidoan/android/ceedee/ui/report/util/MonthYearXAxisValueFormatter.kt
+++ app/src/main/java/com/haidoan/android/ceedee/ui/report/util/MonthYearXAxisValueFormatter.kt
@@ -2,17 +2,17 @@ package com.haidoan.android.ceedee.ui.report.util
 
 import com.github.mikephil.charting.components.AxisBase
 import com.github.mikephil.charting.formatter.ValueFormatter
+import java.time.LocalDate
 
 class MonthYearXAxisValueFormatter(
-    private val startMonth: Int,
-    private val startYear: Int
+    private val starTime: LocalDate,
 ) : ValueFormatter() {
 
     private val MONTH_COUNT_PER_YEAR = 12
 
     override fun getAxisLabel(monthCountFromStart: Float, axis: AxisBase?): String {
-        val resultMonthUnnormalized = startMonth + monthCountFromStart.toInt()
-        var resultYear = startYear + resultMonthUnnormalized / MONTH_COUNT_PER_YEAR
+        val resultMonthUnnormalized = starTime.monthValue + monthCountFromStart.toInt()
+        var resultYear = starTime.year + resultMonthUnnormalized / MONTH_COUNT_PER_YEAR
         var resultMonthNormalized = resultMonthUnnormalized % MONTH_COUNT_PER_YEAR
 
         if (resultMonthNormalized == 0) {
