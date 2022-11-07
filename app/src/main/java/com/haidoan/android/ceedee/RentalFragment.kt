ec1f04fbc4fb387fa3c6fe9aacd75082ea9991d2
ACER <huyhoang432002@gmail.com>
2022-11-07 10:36:39 +0700

    FireStore + change menu + optimized code

--- app/src/main/java/com/haidoan/android/ceedee/RentalFragment.kt
+++ app/src/main/java/com/haidoan/android/ceedee/RentalFragment.kt
@@ -1,19 +1,21 @@
 package com.haidoan.android.ceedee
 
-import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
-import android.view.*
-import android.widget.SearchView
-import androidx.core.view.MenuProvider
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.haidoan.android.ceedee.databinding.FragmentRentalBinding
+import com.haidoan.android.ceedee.fragmentRentalTabs.Adapters.TabRentalAdapter
+import com.haidoan.android.ceedee.fragmentRentalTabs.tabAll
+import com.haidoan.android.ceedee.fragmentRentalTabs.tabComplete
+import com.haidoan.android.ceedee.fragmentRentalTabs.tabInProgress
+import com.haidoan.android.ceedee.fragmentRentalTabs.tabOverdue
 import com.haidoan.android.ceedee.ui.login.AuthenticationViewModel
-import fragmentRentalTabs.*
 
 
 /**
@@ -35,14 +37,14 @@ class RentalFragment : Fragment() {
         _binding = FragmentRentalBinding.inflate(inflater, container, false)
         return binding.root
     }
-    private lateinit var searchViewModel : SearchViewModel
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
       authViewModel = ViewModelProvider(this)[AuthenticationViewModel::class.java]
 
-        var viewPager=view.findViewById<ViewPager>(R.id.viewPagerRental)
-        var tabRental=view.findViewById<TabLayout>(R.id.tabRental)
-        var adapter=tabRentalAdapter(childFragmentManager)
+        val viewPager=view.findViewById<ViewPager>(R.id.viewPagerRental)
+        val tabRental=view.findViewById<TabLayout>(R.id.tabRental)
+        val adapter= TabRentalAdapter(childFragmentManager)
         adapter.addFragment(tabAll(),"All")
         adapter.addFragment(tabInProgress(),"In progress")
         adapter.addFragment(tabOverdue(),"Overdue")
@@ -57,13 +59,10 @@ class RentalFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-
-
     //- ------------------ TODO: test -----------------------------------------
     override fun onStart() {
         super.onStart()
         val currentUser = FirebaseAuth.getInstance().currentUser
-
     }
 
 
--- app/src/main/java/fragmentRentalTabs/Rental.kt
+++
@@ -1,4 +0,0 @@
-package fragmentRentalTabs
-
-data class Rental(var customerId:String?=null,var dueDate:String?=null,var rentDate:String?=null,var returnDate:String?=null,
-                  var status:String?=null,var totalPayment:Float?=null)
