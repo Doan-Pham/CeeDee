7be901a6fe0508fb8bc5feec2b89f9dddb2e15dd
Doan Pham <Doan-Pham@users.noreply.github.com>
2022-11-14 13:03:43 +0700

    Add option menu to DiskRequisitionFragment.kt

--- app/src/main/java/com/haidoan/android/ceedee/ui/disk_screen/disk_requisition/DiskRequisitionFragment.kt
+++ app/src/main/java/com/haidoan/android/ceedee/ui/disk_screen/disk_requisition/DiskRequisitionFragment.kt
@@ -1,10 +1,13 @@
 package com.haidoan.android.ceedee.ui.disk_screen.disk_requisition
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import android.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import com.haidoan.android.ceedee.R
 import com.haidoan.android.ceedee.databinding.FragmentDiskRequisitionBinding
 
 
@@ -23,5 +26,46 @@ class DiskRequisitionFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentDiskRequisitionBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpOptionMenu()
+    }
+
+    private fun setUpOptionMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_disk_requisition, menu)
+
+                val searchView: SearchView =
+                    (menu.findItem(R.id.menu_item_disk_requisition_search).actionView as SearchView)
+                searchView.queryHint = "Type here to search"
+                searchView.maxWidth = Int.MAX_VALUE
+
+                searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                    override fun onQueryTextSubmit(query: String?): Boolean {
+                        return false
+                    }
+
+                    override fun onQueryTextChange(newText: String?): Boolean {
+                        return false
+                    }
+                })
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.menu_item_disk_requisition_search -> {
+                        true
+                    }
+                    R.id.menu_item_disk_requisition_import -> {
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 }
